1b330e2bbbaa4cff417579f2e73dd7333255828f
jose izaias alcantara lima <joseizaias@gmail.com>
2022-08-04 07:58:41 -0300

    refactor: create makSut to a testHelper

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,8 +10,11 @@ describe('Account MongoDB Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
 
     const account = await sut.add({
       name: 'any_name',
