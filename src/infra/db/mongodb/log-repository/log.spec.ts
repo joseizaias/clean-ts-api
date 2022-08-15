012457ec7543a0d48242334f13e69e6118973d4a
jose izaias alcantara lima <joseizaias@gmail.com>
2022-08-15 04:42:38 -0300

    refactor: ensure LogMongoRepository creates an error log on success

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -1,7 +1,10 @@
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { LogoMongoRepository } from './log'
+import { LogMongoRepository } from './log'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
 
@@ -20,7 +23,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an error log on success', async () => {
-    const sut = new LogoMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
