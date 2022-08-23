b7f14f6204632a605ac5e02116b3f258db66ecd7
jose izaias alcantara lima <joseizaias@gmail.com>
2022-08-23 07:46:16 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { Validation } from '../../presentation/helpers/validators/validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
 
@@ -9,9 +10,12 @@ describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation()
     const validations: Validation[] = []
+
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
     }
+
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
---
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from '../../errors'
+import { Validation } from './validation'
+
+export class CompareFieldsValidation implements Validation {
+  private readonly fieldName: string
+  private readonly fieldNameCompare: string
+
+  constructor (fieldName: string, fieldNameCompare: string) {
+    this.fieldName = fieldName
+    this.fieldNameCompare = fieldNameCompare
+  }
+
+  validate (input: any): Error | null {
+    if (input[this.fieldName] !== input[this.fieldNameCompare]) {
+      return new InvalidParamError(this.fieldName)
+    }
+    return null
+  }
+}
