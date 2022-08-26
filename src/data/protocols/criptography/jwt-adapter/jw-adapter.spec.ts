08bdb5c7f70db3476276407be6eb52b7b8a4654e
jose izaias alcantara lima <joseizaias@gmail.com>
2022-08-26 13:03:03 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/data/protocols/criptography/jwt-adapter/jw-adapter.spec.ts
+++ src/data/protocols/criptography/jwt-adapter/jw-adapter.spec.ts
@@ -20,4 +20,16 @@ describe('JWT Adapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  test('Should throws if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    // jest.spyOn(jwt, 'sign').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    // a linha acima não funciona porque tem vários tipos de retorno e isso causa confusão para TypeScript. SEndo assim,
+    // temos que usar o mockImplementationOnce(), conforme abaixo
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
