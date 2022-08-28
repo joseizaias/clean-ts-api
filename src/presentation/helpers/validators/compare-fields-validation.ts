1aaed1d5b7bb93f0249e4c012b7d8da12bb497cb
jose izaias alcantara lima <joseizaias@gmail.com>
2022-08-28 16:36:03 -0300

    refactor: remove duplicate code

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -2,13 +2,10 @@ import { InvalidParamError } from '../../errors'
 import { Validation } from '../../protocols/validation'
 
 export class CompareFieldsValidation implements Validation {
-  private readonly fieldName: string
-  private readonly fieldNameCompare: string
-
-  constructor (fieldName: string, fieldNameCompare: string) {
-    this.fieldName = fieldName
-    this.fieldNameCompare = fieldNameCompare
-  }
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldNameCompare: string
+  ) {}
 
   validate (input: any): Error | null {
     if (input[this.fieldName] !== input[this.fieldNameCompare]) {
