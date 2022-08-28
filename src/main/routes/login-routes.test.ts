e272acb197e65c670613cdadbcc7aca0838fd0f9
jose izaias alcantara lima <joseizaias@gmail.com>
2022-08-28 18:34:34 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,11 @@
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 import request from 'supertest'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { app } from '../config/app'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -14,7 +18,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -27,6 +31,25 @@ describe('Login Routes', () => {
           email: 'joseizaias1@gmail.com',
           password: '1234',
           passwordConfirmation: '1234'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const salt = 12
+      const password = await hash('1234', salt)
+      await accountCollection.insertOne({
+        name: 'jose izaias1',
+        email: 'joseizaias1@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'joseizaias1@gmail.com',
+          password: '1234'
         })
         .expect(200)
     })
