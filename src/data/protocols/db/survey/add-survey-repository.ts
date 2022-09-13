f64fa3f5f53ddca9266371f0404528e22d5cb0f7
jose izaias alcantara lima <joseizaias@gmail.com>
2022-09-13 18:40:40 -0300

    feat: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -0,0 +1,6 @@
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+
+export interface AddSurveyRepository {
+  // eslint-disable-next-line @typescript-eslint/method-signature-style
+  add (surveyData: AddSurveyModel): Promise<void>
+}
