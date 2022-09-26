307fc3de895ce4ad13850d4b1ed31da6c5019fec
jose izaias alcantara lima <joseizaias@gmail.com>
2022-09-25 21:23:52 -0300

    test: add middleware interface adjust test

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,5 +1,6 @@
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
+import { AuthMiddleware } from './auth-middleware'
 
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
