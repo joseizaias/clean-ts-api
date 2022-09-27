21caaf24906e43a58ea05cf114575c525e28d75a
jose izaias alcantara lima <joseizaias@gmail.com>
2022-09-27 08:09:57 -0300

    test: ensure JwtAdapter calls verify with correct values

--- src/infra/criptography/jwt-adapter/jw-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jw-adapter.spec.ts
@@ -4,6 +4,10 @@ import { JwtAdapter } from './jwt-adapter'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return new Promise(resolve => resolve('any_token'))
+  },
+
+  async verify (value: string): Promise<string> {
+    return new Promise(resolve => resolve('any_value'))
   }
 }))
 
@@ -37,5 +41,32 @@ describe('JWT Adapter', () => {
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
     })
+  })
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
+
+    // test('Should returns a token on sign success', async () => {
+    //   const sut = makeSut()
+    //   const accessToken = await sut.encrypt('any_id')
+    //   expect(accessToken).toBe('any_token')
+    // })
+
+    // test('Should throws if sign throws', async () => {
+    //   const sut = makeSut()
+    //   // jest.spyOn(jwt, 'sign').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    //   // a linha acima não funciona porque tem vários tipos de retorno e isso causa confusão para TypeScript. SEndo assim,
+    //   // temos que usar o mockImplementationOnce(), conforme abaixo
+    //   jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+    //     throw new Error()
+    //   })
+    //   const promise = sut.encrypt('any_id')
+    //   await expect(promise).rejects.toThrow()
+    // })
   })
 })
