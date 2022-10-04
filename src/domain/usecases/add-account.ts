b869a7bd2a4c2977a929ae42a549207590892714
jose izaias alcantara lima <joseizaias@gmail.com>
2022-10-03 21:44:50 -0300

    refactor: change models to use Omit

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,10 +1,11 @@
 import { AccountModel } from '@/domain/models/account'
 
-export type AddAccountModel = {
-  name: string
-  email: string
-  password: string
-}
+export type AddAccountModel = Omit<AccountModel, 'id'>
+// export type AddAccountModel = {
+//   name: string
+//   email: string
+//   password: string
+// }
 
 export interface AddAccount {
   // eslint-disable-next-line @typescript-eslint/method-signature-style
--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,10 +1,11 @@
-import { SurveyAnswerModel } from '@/domain/models/survey'
+import { SurveyModel } from '@/domain/models/survey'
 
-export type AddSurveyModel = {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export type AddSurveyModel = Omit<SurveyModel, 'id'>
+// export type AddSurveyModel = {
+//   question: string
+//   answers: SurveyAnswerModel[]
+//   date: Date
+// }
 
 export interface AddSurvey {
   // eslint-disable-next-line @typescript-eslint/method-signature-style
