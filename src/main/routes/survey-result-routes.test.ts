e66ae6e64b4b35632a95eb02845f3c88621a2100
jose izaias alcantara lima <joseizaias@gmail.com>
2022-10-08 08:08:25 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,8 +1,69 @@
+import { sign } from 'jsonwebtoken'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { app } from '@/main/config/app'
+import env from '@/main/config/env'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const addedUser = await accountCollection.insertOne({
+    name: 'jose izaias',
+    email: 'joseizaias@gmail.com',
+    password: '1234'
+  })
+  const id = addedUser.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  await surveyCollection.insertMany([{
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }, {
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  },{
+    question: 'other_question',
+    answers: [{
+      image: 'other_image',
+      answer: 'other_answer'
+    }, {
+      answer: 'another_answer'
+    }],
+    date: new Date()
+  }])
+
+  return accessToken
+}
 
 describe('Login Routes', () => {
+  beforeAll(async () => {
+    // console.log(global.__MONGO_URI__)
+    await MongoHelper.connect(global.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   describe('PUT /surveys/:surveyId/result', () => {
     test('Should return 403 on save surveys result without accessToken', async () => {
       await request(app)
@@ -11,6 +72,27 @@ describe('Login Routes', () => {
           answer: 'any_answer'
         })
         .expect(403)
+    })
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          image: 'http://image-name.com',
+          answer: 'answer 1'
+        }, {
+          answer: 'answer 2'
+        }],
+        date: new Date()
+      })
+      const accessToken = await makeAccessToken()
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'answer 1'
+        })
+        .expect(200)
     })
   })
 })
