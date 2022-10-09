11c256565f2f267cc2c96543b86debf86f9a8c57
jose izaias alcantara lima <joseizaias@gmail.com>
2022-10-09 17:45:36 -0300

    feat: apply no-cache middleware on swagger api

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -2,7 +2,7 @@ export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API - TypeScript',
-    description: 'API para realizar enquetes entre devs',
+    description: 'API para realizar enquetes entre Devs!',
     version: '1.0.0'
   }
 }
