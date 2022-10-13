1e601c7ef8c6a87e0238acc608ef5573ea217203
jose izaias alcantara lima <joseizaias@gmail.com>
2022-10-12 22:13:42 -0300

    test: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -54,6 +54,13 @@ describe('DbLoadSurveyResult UseCase', () => {
     expect(loadByIdSpy).toHaveBeenCalledWith('any_surveyId')
   })
 
+  test('Should return surveyResultModel with all answers with count 0 if LoadSurveyResultRepository returns null', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockReturnValueOnce(Promise.resolve(null as unknown as SurveyResultModel))
+    const surveyResult = await sut.load('any_surveyId')
+    expect(surveyResult).toEqual(mockSurveyResultModel())
+  })
+
   test('Should return surveyResultModel on sucess', async () => {
     const { sut } = makeSut()
     const surveyResult = await sut.load('any_surveyId')
